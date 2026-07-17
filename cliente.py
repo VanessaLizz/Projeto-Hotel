04b3837e6d3c058cfe511de587651f83ab2b6212
Vanessa Sousa <vanessalizsousa1996@gmail.com>
2026-07-17 10:05:45 -0300

    estrturação do projeto e separação do molde inicial em arquivos distintos

---
+++ cliente.py
@@ -0,0 +1,6 @@
+class cliente:
+    def __init__(self, nome: str, telefone: str, email: str, id_unico: int):
+        self.nome = nome
+        self.telefone = telefone
+        self.email = email
+        self.id_unico = id_unico
---
+++ quarto.py
@@ -0,0 +1,6 @@
+class quarto():
+  def __init__(self, numero_quarto: int, tipo_quarto: str, preco_diaria: float):
+    self.numero_quarto = numero_quarto
+    self.tipo_quarto = tipo_quarto
+    self.preco_diaria = preco_diaria
+    self.disponibilidade = True
